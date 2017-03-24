8a0b244fa04de9269264300ff5c8465d8c43c68c
System <none>
2017-03-24 15:10:32 +0000

    Exporting to Commit451/skyhook

--- gitlab.js
+++ gitlab.js
@@ -1,9 +1,14 @@
 // gitlab.js
+// https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/user/project/integrations/webhooks.md
 // ========
 module.exports = {
   parse: function (req, discordPayload) {
-    // whatever
     var body = req.body
+    var type = body.object_kind
+    switch (type) {
+      case "push":
+        break;
+    }
     discordPayload.content = body.before;
   }
 };
