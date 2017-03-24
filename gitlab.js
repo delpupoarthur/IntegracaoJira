0210fee4d3b6b6c43f1e34b34f1f1c2dbe219c5c
System <none>
2017-03-24 15:33:01 +0000

    Checkpoint

--- gitlab.js
+++ gitlab.js
@@ -4,11 +4,16 @@
 module.exports = {
   parse: function (req, discordPayload) {
     var body = req.body
+    var username = body.user_name
+    var url = body.project.web_url
     var type = body.object_kind
+    var projectName = body.project.name
+    var numberOfCommits = body.total_commits_count
     switch (type) {
       case "push":
+        discordPayload.content = username + " pushed " + numberOfCommits + " commit(s) to " + projectName + "\n" + url;
         break;
     }
-    discordPayload.content = body.before;
+    
   }
 };
