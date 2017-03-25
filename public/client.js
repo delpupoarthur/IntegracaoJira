6f66a83b555d4b1b7dad12601a526445495d8e46
System <none>
2017-03-25 06:51:52 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -1,9 +1,55 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
 
-$(function() {
-  console.log('hello world :o');
-});
+window.mdc.autoInit();
+    
+(function(global) {
+    'use strict';
+
+    var MDCSnackbar = global.mdc.snackbar.MDCSnackbar;
+    var snackbar = new MDCSnackbar(document.getElementById('mdc-js-snackbar'));
+
+    var show = function(sb) {
+        var data =  {
+          message: "URL Generated. Copied to Clipboard",
+          multiline: false
+        };
+
+        sb.show(data);
+    }
+
+    document.getElementById('greeting-form').addEventListener('submit', function(evt) {
+      evt.preventDefault();
+      var discordHookUrl = evt.target.elements.url.value;
+      if (discordHookUrl) {
+        discordHookUrl = discordHookUrl.replace("discordapp.com", "skyhook.glitch.me")
+        //add the provider
+        var index = select.selectedIndex
+        switch(index) {
+          case 0:
+            discordHookUrl = discordHookUrl + "/appveyor"
+            break;
+          case 1:
+            discordHookUrl = discordHookUrl + "/circleci"
+            break;
+          case 2:
+            discordHookUrl = discordHookUrl + "/gitlab"
+            break;
+          case 3:
+            discordHookUrl = discordHookUrl + "/travis"
+            break;
+        }
+
+      }
+      console.log(discordHookUrl)
+      show(snackbar);
+
+      window.copyToClipboard(discordHookUrl);
+    });
+
+    var MDCSelect = mdc.select.MDCSelect;
+    var selectElement = document.getElementById('js-select');
+    var select = MDCSelect.attachTo(selectElement);
+
+  })(this);
