04bcc9516968e4095481231e69d4dd0fb4166df2
System <none>
2017-05-12 17:40:43 +0000

    Import of Commit451/skyhook

--- providers/unity.js
+++ providers/unity.js
@@ -3,54 +3,50 @@
 // ========
 module.exports = {
     parse: function (req, discordPayload) {
-        var body = req.body;
-        var hookID = body.hookId;
+        const body = req.body;
 
         discordPayload.setEmbedColor(0x222C37);
 
-        if (hookID == null) {
-            var projectName = body.projectName;
-            var projectTarget = body.buildTargetName;
-            var projectVersion = body.buildNumber;
-            var share = body.links.share_url;
-            var type = body.buildStatus;
-            var ref = body.ref;
-            var content = "No download available.";
-            var user = {
-                name: projectTarget,
-                icon_url: "https://developer.cloud.unity3d.com/images/icon-default.png"
-            };
-            var download = "";
-            var link = "";
-            discordPayload.setUser(projectName + "Buildserver", "https://developer.cloud.unity3d.com/images/icon-default.png");
-            switch (type) {
-                case "success":
-                    if (share != null) {
-                        download = share.href;
-                        content = "[`Download it here`](" + download + ")";
-                    }
-                    var link = "";
-                    content = "**New build**\n" + content;
-                    break;
-                case "queued":
-                    content = "**In build queue**\nIt will be update to version  #" + projectVersion + "\n";
-                    break;
-                case "started":
-                    content = "**Build is started**\nBuilding version  #" + projectVersion + "\n";
-                    break;
-                case "failed":
-                    content = "**Build failed**\n" + "Latest version is still  #" + (projectVersion - 1) + "\n";
-                    break;
 
-            }
-            discordPayload.addEmbed({
-                title: "[" + projectName + "] " + " version #" + projectVersion,
-                url: download,
-                author: user,
-                description: content
-            });
-        } else {
-            discordPayload.setContent("**Ping from host!**");
+        const projectName = body.projectName;
+        const projectTarget = body.buildTargetName;
+        const projectVersion = body.buildNumber;
+        const share = body.links.share_url;
+        const type = body.buildStatus;
+        const ref = body.ref;
+        let content = "No download available.";
+        const user = {
+            name: projectTarget,
+            icon_url: "https://developer.cloud.unity3d.com/images/icon-default.png"
+        };
+        let download = "";
+        let link = "";
+        discordPayload.setUser(projectName + "Buildserver", "https://developer.cloud.unity3d.com/images/icon-default.png");
+        switch (type) {
+            case "success":
+                if (share !== null) {
+                    download = share.href;
+                    content = "[`Download it here`](" + download + ")";
+                }
+                link = "";
+                content = "**New build**\n" + content;
+                break;
+            case "queued":
+                content = "**In build queue**\nIt will be update to version  #" + projectVersion + "\n";
+                break;
+            case "started":
+                content = "**Build is started**\nBuilding version  #" + projectVersion + "\n";
+                break;
+            case "failed":
+                content = "**Build failed**\n" + "Latest version is still  #" + (projectVersion - 1) + "\n";
+                break;
+
         }
+        discordPayload.addEmbed({
+            title: "[" + projectName + "] " + " version #" + projectVersion,
+            url: download,
+            author: user,
+            description: content
+        });
     }
 };
--- test/heroku.js
+++ test/heroku.js
@@ -1,9 +1,9 @@
 process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../server');
-var should = chai.should();
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
 
 chai.use(chaiHttp);
 
@@ -20,7 +20,7 @@ describe('/POST heroku', () => {
 				res.should.have.status(200);
 				console.log(res.body);
 				res.body.should.be.a('object');
-				res.body.should.have.property('embeds')
+				res.body.should.have.property('embeds');
 				done();
 			});
 	});
