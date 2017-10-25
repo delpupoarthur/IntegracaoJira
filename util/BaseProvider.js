c2a163bc653efc09739eb02ced51288c6a52a086
Glitch (skyhook) <none>
2017-10-25 20:42:17 +0000

    Import of Commit451/skyhook

--- util/BaseProvider.js
+++ util/BaseProvider.js
@@ -6,24 +6,25 @@ class BaseProvider {
         this.payload = new DiscordPayload();
     }
 
-    async parse(req){
+    async parse(req) {
         this.req = req;
         this.body = req.body;
 
         let type = 'parseData';
-        if(typeof this['getType'] !== 'undefined'){
+        if (typeof this['getType'] !== 'undefined') {
             type = await this['getType']();
         }
         type = BaseProvider.formatType(type);
 
-        if(typeof this[type] !== 'undefined'){
+        if (typeof this[type] !== 'undefined') {
+            console.log('[' + (new Date()).toUTCString() + '] Calling ' + type + '() in ' + this.constructor.name + ' provider.')
             await this[type]();
         }
 
         return this.payload.getData();
     }
 
-    static formatType(type){
+    static formatType(type) {
         type = type.replace(/:/, '_'); // needed because of BitBucket
         return camel(type);
     }
